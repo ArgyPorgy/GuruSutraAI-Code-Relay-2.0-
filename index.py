e83902d81fa2d5c61540680301ca4d814bac1274
ArgyPorgy <carghya10@gmail.com>
2023-07-17 01:40:46 +0530

    made some changes in the dataset and the index.py

--- index.py
+++ index.py
@@ -23,21 +23,21 @@ def generate_quote(category=None):
 def render():
     return render_template('index.html', generated_quote=None)
 
-# @app.route('/')
-# def render():
-#     return render_template('index.html', generated_quote=None)
-
 @app.route('/generate', methods=['POST'])
 def generate():
     prompt = request.form.get('prompt')
-
-    if "sad" in prompt.lower():
-        generated_quote = generate_quote(category="sadness")
-    elif "depressed" in prompt.lower():
-        generated_quote = generate_quote(category="spirituality")
-    elif "good enough" in prompt.lower():
-        generated_quote = generate_quote(category="insecurity")
+    
+    if prompt.strip():  
+        if "sad" in prompt.lower(): 
+            generated_quote = generate_quote(category="sadness")
+        elif "depressed" in prompt.lower():
+            generated_quote = generate_quote(category="spirituality")
+        elif "good enough" in prompt.lower():
+            generated_quote = generate_quote(category="insecurity")
+        else:
+            generated_quote = generate_quote()
     else:
+        # If the input is empty, set the response text to an empty string
         generated_quote = ""
 
     return render_template('index.html', generated_quote=generated_quote)
